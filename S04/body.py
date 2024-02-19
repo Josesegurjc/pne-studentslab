0e8e1875339b6332889ad9cd188e08f96f740f24
josesegurjc <joseseguramorales233@gmail.com>
2024-02-19 11:46:15 +0100

    Corrections

--- S04/body.py
+++ S04/body.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-file_info = Path("../../P01/U5.txt").read_text()
+file_info = Path("Sequences/U5.txt").read_text()
 file_info = file_info.split("\n")[1:]
 print("Body of the U5.txt file:")
 for e in file_info:
--- S04/sequence.py
+++ S04/sequence.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-file_info = Path("../../P01/ADA.txt").read_text()
+file_info = Path("Sequences/ADA.txt").read_text()
 file_info = file_info[file_info.index("\n"):]
 file_info = file_info.replace("\n", "")
 print(len(file_info))
