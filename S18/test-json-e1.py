875c9c79242ba014bb30f28c0ce99b3df4871a2b
josesegurjc <joseseguramorales233@gmail.com>
2024-04-17 12:46:15 +0200

    S18

---
+++ S18/test-json-e1.py
@@ -0,0 +1,63 @@
+
+import http.client
+import json
+import termcolor
+
+PORT = 8081
+SERVER = 'localhost'
+
+print(f"\nConnecting to server: {SERVER}:{PORT}\n")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER, PORT)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", "/listusers")
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+# -- Create a variable with the data,
+# -- form the JSON received
+person = json.loads(data1)
+list_of_dicts = person["People"]
+
+print("CONTENT: ")
+
+# Print the information in the object
+for i in range(0, len(list_of_dicts)):
+    print()
+    user = list_of_dicts[i]
+    termcolor.cprint("Name: ", 'green', end="")
+    print(user['Firstname'], user['Lastname'])
+
+    termcolor.cprint("Age: ", 'green', end="")
+    print(user['age'])
+
+    # Get the phoneNumber list
+    phoneNumbers = user['phoneNumber']
+
+    # Print the number of elements int the list
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+
+    # Print all the numbers
+    for i, num in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone {}:".format(i), 'blue')
+
+        # The element num contains 2 fields: number and type
+        termcolor.cprint("    Type: ", 'red', end='')
+        print(num['type'])
+        termcolor.cprint("    Number: ", 'red', end='')
+        print(num['number'])
